80d645908308de7854ea031ed1867fe4d80b5cca
Yassine Boufenneche <yboufenneche@gmail.com>
2021-10-23 23:22:21 +0200

    add cockatil details

---
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Cocktail } from '../interfaces/cocktail.interface';
+
+@Component({
+  selector: 'app-cocktail-list',
+  templateUrl: './cocktail-list.component.html',
+  styleUrls: ['./cocktail-list.component.scss'],
+})
+export class CocktailListComponent implements OnInit {
+  // list of cocktails
+  cocktails: Cocktail[] = [
+    {
+      name: 'Mojito',
+      img: 'https://www.hangoverweekends.co.uk/media/15505/mojito.jpg?width=500&height=375',
+      description:
+        'The Mojito complimenting summer perfectly with a fresh minty taste. The mixture of white rum, mint, lime juice, sugar and soda water is crisp and clean with a relatively low alcohol content, the soda water can be replaced with sprite or 7-up. When preparing a mojito always crush the mint leaves as opposed to dicing to unlock oils that will assist with enhancing the minty flavour.',
+    },
+    {
+      name: 'Mojito',
+      img: 'https://www.hangoverweekends.co.uk/media/15505/mojito.jpg?width=500&height=375',
+      description:
+        'The Mojito complimenting summer perfectly with a fresh minty taste. The mixture of white rum, mint, lime juice, sugar and soda water is crisp and clean with a relatively low alcohol content, the soda water can be replaced with sprite or 7-up. When preparing a mojito always crush the mint leaves as opposed to dicing to unlock oils that will assist with enhancing the minty flavour.',
+    },
+    {
+      name: 'Mojito',
+      img: 'https://www.hangoverweekends.co.uk/media/15505/mojito.jpg?width=500&height=375',
+      description:
+        'The Mojito complimenting summer perfectly with a fresh minty taste. The mixture of white rum, mint, lime juice, sugar and soda water is crisp and clean with a relatively low alcohol content, the soda water can be replaced with sprite or 7-up. When preparing a mojito always crush the mint leaves as opposed to dicing to unlock oils that will assist with enhancing the minty flavour.',
+    },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
