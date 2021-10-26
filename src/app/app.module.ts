c6dc91dad788ae08e1127a31bd076186fe2b77ea
Yassine Boufenneche <yboufenneche@gmail.com>
2021-10-26 22:40:12 +0200

    using input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
-import { CocktailDetailsComponent } from './cocktail-details/cocktail-details.component';
+import { CocktailListComponent } from './cocktails-container/cocktail-list/cocktail-list.component';
+import { CocktailDetailsComponent } from './cocktails-container/cocktail-details/cocktail-details.component';
+import { CocktailsContainerComponent } from './cocktails-container/cocktails-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CocktailListComponent,
-    CocktailDetailsComponent
+    CocktailDetailsComponent,
+    CocktailsContainerComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/cocktails-container/cocktails-container.component.ts
+++ src/app/cocktails-container/cocktails-container.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Cocktail } from '../interfaces/cocktail.interface';
 
 @Component({
-  selector: 'app-cocktail-list',
-  templateUrl: './cocktail-list.component.html',
-  styleUrls: ['./cocktail-list.component.scss'],
+  selector: 'app-cocktails-container',
+  templateUrl: './cocktails-container.component.html',
+  styleUrls: ['./cocktails-container.component.scss'],
 })
-export class CocktailListComponent implements OnInit {
+export class CocktailsContainerComponent implements OnInit {
   // list of cocktails
   cocktails: Cocktail[] = [
     {
@@ -28,6 +28,7 @@ export class CocktailListComponent implements OnInit {
         'The Mojito complimenting summer perfectly with a fresh minty taste. The mixture of white rum, mint, lime juice, sugar and soda water is crisp and clean with a relatively low alcohol content, the soda water can be replaced with sprite or 7-up. When preparing a mojito always crush the mint leaves as opposed to dicing to unlock oils that will assist with enhancing the minty flavour.',
     },
   ];
+  public selectedCocktail: Cocktail = this.cocktails[0];
   constructor() {}
 
   ngOnInit(): void {}
